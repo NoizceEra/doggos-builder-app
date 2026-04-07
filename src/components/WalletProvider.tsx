ea27116c904383b4197d1b5b535da9d137e82148
Doggos Builder <builder@doggos.local>
2026-04-06 18:17:13 -0700

    fix: replace UnsafeBurnerWalletAdapter with PhantomWalletAdapter for production

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -6,7 +6,7 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
@@ -36,7 +36,7 @@ export default function ClientWalletProvider({
        * instantiate its legacy adapter here. Common legacy adapters can be found
        * in the npm package `@solana/wallet-adapter-wallets`.
        */
-      new UnsafeBurnerWalletAdapter(),
+      new PhantomWalletAdapter(),
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [network]
